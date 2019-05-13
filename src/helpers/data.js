30a6bd2ac56dbfe507986d301617f01a02bbb0d2
unknown <imed_jebari@hotmail.fr>
2019-05-13 03:04:10 +0200

    all global data/var 🌍 ..

---
+++ src/helpers/data.js
@@ -0,0 +1,18 @@
+
+const __GOOGLE_TRANSLATE_LINK__ = 'https://translate.google.com/' ;
+const __NUMBER_OF_ALL_LANGUAGES__ = 104 ;
+const __SAVE_FILE_PATH__ = '../data/cluster/languages.js' ;
+const __INPUT_CSV_FILE_PATH__ = '../data/input/input.csv' ;
+const __OUTPUT_CSV_FILE_PATH__ = '../data/output/output.csv' ;
+const __RUN_SCRAPE_SCRIPT__ = "npm run scrape" ;
+const __RUN_TRANSLATE_SCRIPT__ = "npm run translate" ;
+
+module.exports = {
+                    __NUMBER_OF_ALL_LANGUAGES__ ,
+                    __GOOGLE_TRANSLATE_LINK__ ,
+                    __SAVE_FILE_PATH__ ,
+                    __INPUT_CSV_FILE_PATH__ ,
+                    __OUTPUT_CSV_FILE_PATH__ ,
+                    __RUN_SCRAPE_SCRIPT__ ,
+                    __RUN_TRANSLATE_SCRIPT__
+                  };
